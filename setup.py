98c290f7c7cac45b8fa89c2af493b75a74e9ec2f
Wilco van Duinkerken <wilco@sparkboxx.com>
2014-02-25 22:30:40 +0100

    Fixed package dir in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from distutils.core import setup
 
+
 setup(name='VUA_pylib',
-      version='1.0',
+      version='1.1',
       description = 'Various KAF / NAF python helpers',
       author = 'Ruben Izquierdo',
       author_email = 'r.izquierdobevia@vu.nl',
-      packages = ['VUA_pylib.lexicon','VUA_pylib.common',
-        'VUA_pylib.io','VUA_pylib.corpus_reader']
+      package_dir = {'':''},
+      packages = ['lexicon','common','io','corpus_reader'],
       )
