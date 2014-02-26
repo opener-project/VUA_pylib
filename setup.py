553994dd7aff96ba845aba6e6f361e4c8b0511cf
Wilco van Duinkerken <wilco@sparkboxx.com>
2014-02-26 13:17:23 +0100

    Adjusted setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from distutils.core import setup
 
 
 setup(name='VUA_pylib',
-      version='1.1',
+      version='1.3',
       description = 'Various KAF / NAF python helpers',
       author = 'Ruben Izquierdo',
       url = 'https://github.com/cltl/VUA_pylib',
       author_email = 'r.izquierdobevia@vu.nl',
-      package_dir = {'':''},
-      packages = ['lexicon','common','io_utils','corpus_reader'],
+      packages = ['VUA_pylib','VUA_pylib.lexicon','VUA_pylib.common','VUA_pylib.io_utils','VUA_pylib.corpus_reader'],
       )
