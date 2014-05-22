08087f17fefc1eb0e8d41afdbd3b3a3a951da1ef
Yorick Peterse <yorickpeterse@olery.com>
2014-05-22 14:14:45 +0200

    Include everything in VUA_pylib in the package.

--- setup.py
+++ setup.py
@@ -1,11 +1,24 @@
+import os
+
 from distutils.core import setup
 
+# Get all the directories for the package that lazy way. This way we don't have
+# to also manually update that list.
+lib_name    = 'VUA_pylib'
+directories = [lib_name]
 
-setup(name='VUA_pylib',
-      version='1.4',
-      description = 'Various KAF / NAF python helpers',
-      author = 'Ruben Izquierdo',
-      url = 'https://github.com/cltl/VUA_pylib',
-      author_email = 'r.izquierdobevia@vu.nl',
-      packages = ['VUA_pylib','VUA_pylib.lexicon','VUA_pylib.common','VUA_pylib.io_utils','VUA_pylib.corpus_reader'],
-      )
+for name in os.listdir(lib_name):
+    full_path = os.path.join(lib_name, name)
+
+    if os.path.isdir(full_path):
+        directories.append('%s.%s' % (lib_name, name))
+
+setup(
+    name         = lib_name,
+    version      = '1.5',
+    description  = 'Various KAF / NAF python helpers',
+    author       = 'Ruben Izquierdo',
+    url          = 'https://github.com/cltl/VUA_pylib',
+    author_email = 'r.izquierdobevia@vu.nl',
+    packages     = directories,
+)
