ac3a708028fdc821ccc0d71d41d187cde33c5702
Yorick Peterse <yorickpeterse@olery.com>
2014-05-22 14:27:43 +0200

    Use full paths in the setup.py.
    This should make sure that Pip doesn't mess things up.

--- setup.py
+++ setup.py
@@ -4,10 +4,12 @@ from distutils.core import setup
 
 # Get all the directories for the package that lazy way. This way we don't have
 # to also manually update that list.
+this_folder = os.path.dirname(os.path.realpath(__file__))
 lib_name    = 'VUA_pylib'
+lib_dir     = os.path.join(this_folder, lib_name)
 directories = [lib_name]
 
-for name in os.listdir(lib_name):
+for name in os.listdir(lib_dir):
     full_path = os.path.join(lib_name, name)
 
     if os.path.isdir(full_path):
